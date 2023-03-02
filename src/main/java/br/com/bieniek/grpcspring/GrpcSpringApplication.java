6599ed4533a446ddef0e800d9e2bb097b68fe832
Bianeck <thiagobianeck@gmail.com>
2023-03-02 09:22:16 -0300

    Initial commit

---
+++ src/main/java/br/com/bieniek/grpcspring/GrpcSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.grpcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bieniek/grpcspring/GrpcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.grpcspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
